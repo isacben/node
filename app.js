5d1227b6511fdde2ea366c7fbc057920f6a0b8aa
Isaac Benitez <ibsandoval@gmail.com>
2020-09-27 18:34:06 -0700

    Init repository and push to GitHub

---
+++ app.js
@@ -0,0 +1,34 @@
+const { static } = require('express');
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
+
+// express app
+const app = express();
+
+// middelware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+
+// register view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = 'mongodb+srv://admin:admin@cluster0.xtjky.mongodb.net/abbertodb?retryWrites=true&w=majority';
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
+
+// routes
+app.get('*', checkUser);
+app.get('/', (req, res) => {
+    res.render('index');
+})
+app.get('/dashboard', requireAuth, (req, res) => {
+    res.render('admin/index');
+});
+
+app.use(authRoutes);
